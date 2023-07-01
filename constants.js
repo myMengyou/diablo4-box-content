6cfaa21350a53c8d8d6b2208305d94ebfe2d5149
myMengyou <38711918+myMengyou@users.noreply.github.com>
2023-07-01 16:13:32 +0800

    feat: 初步完成。

---
+++ constants.js
@@ -0,0 +1,18 @@
+const STORAGE_KEY = {
+    powerData: "DIABLO4_POWER_DATA",
+    itemData: "DIABLO4_ITEM_DATA",
+}
+const POWER_TYPE_LIST = [
+    {
+        label: "无差别类型",
+        value: 1,
+    },
+    {
+        label: "范围差别类型",
+        value: 2,
+    },
+    {
+        label: "强度数值差别类型",
+        value: 3,
+    },
+]
